801e2393dd9eaceb5c3e54226a9beef3948e2b48
Trevor <Trevor@DESKTOP-1KRRRE2>
2018-03-07 14:25:24 -0500

    Add project files.

---
+++ SemesterAverage/SemesterAverage.Designer.vb
@@ -0,0 +1,415 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSemesterAverage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tbLetterGrade6 = New System.Windows.Forms.TextBox()
+        Me.tbLetterGrade5 = New System.Windows.Forms.TextBox()
+        Me.tbLetterGrade4 = New System.Windows.Forms.TextBox()
+        Me.tbLetterGrade3 = New System.Windows.Forms.TextBox()
+        Me.tbLetterGrade2 = New System.Windows.Forms.TextBox()
+        Me.tbLetterGrade1 = New System.Windows.Forms.TextBox()
+        Me.tbMark6 = New System.Windows.Forms.TextBox()
+        Me.tbMark5 = New System.Windows.Forms.TextBox()
+        Me.tbMark4 = New System.Windows.Forms.TextBox()
+        Me.tbMark3 = New System.Windows.Forms.TextBox()
+        Me.tbMark2 = New System.Windows.Forms.TextBox()
+        Me.tbMark1 = New System.Windows.Forms.TextBox()
+        Me.lbCourse6 = New System.Windows.Forms.Label()
+        Me.lbCourse5 = New System.Windows.Forms.Label()
+        Me.lbCourse4 = New System.Windows.Forms.Label()
+        Me.lbCourse3 = New System.Windows.Forms.Label()
+        Me.lbCourse2 = New System.Windows.Forms.Label()
+        Me.lbCouse1 = New System.Windows.Forms.Label()
+        Me.lbSemester = New System.Windows.Forms.Label()
+        Me.tbSemester = New System.Windows.Forms.TextBox()
+        Me.tbSemesterGrade = New System.Windows.Forms.TextBox()
+        Me.tbMessages = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SemesterGradeToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Course6MarkToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Course5MarkToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Course4MarkToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.CourseMark3ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.CourseMark2ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.CourseMark1ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.CalculateButtonToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ResetButtonToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ExitButtonToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'tbLetterGrade6
+        '
+        Me.tbLetterGrade6.AccessibleName = "tbLetterGrade6"
+        Me.tbLetterGrade6.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade6.Location = New System.Drawing.Point(219, 217)
+        Me.tbLetterGrade6.Name = "tbLetterGrade6"
+        Me.tbLetterGrade6.ReadOnly = True
+        Me.tbLetterGrade6.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade6.TabIndex = 0
+        Me.tbLetterGrade6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbLetterGrade5
+        '
+        Me.tbLetterGrade5.AccessibleName = "tbLetterGrade5"
+        Me.tbLetterGrade5.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade5.Location = New System.Drawing.Point(219, 174)
+        Me.tbLetterGrade5.Name = "tbLetterGrade5"
+        Me.tbLetterGrade5.ReadOnly = True
+        Me.tbLetterGrade5.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade5.TabIndex = 0
+        Me.tbLetterGrade5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbLetterGrade4
+        '
+        Me.tbLetterGrade4.AccessibleName = "tbLetterGrade4"
+        Me.tbLetterGrade4.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade4.Location = New System.Drawing.Point(219, 133)
+        Me.tbLetterGrade4.Name = "tbLetterGrade4"
+        Me.tbLetterGrade4.ReadOnly = True
+        Me.tbLetterGrade4.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade4.TabIndex = 0
+        Me.tbLetterGrade4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbLetterGrade3
+        '
+        Me.tbLetterGrade3.AccessibleName = "tbLetterGrade3"
+        Me.tbLetterGrade3.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade3.Location = New System.Drawing.Point(219, 95)
+        Me.tbLetterGrade3.Name = "tbLetterGrade3"
+        Me.tbLetterGrade3.ReadOnly = True
+        Me.tbLetterGrade3.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade3.TabIndex = 0
+        Me.tbLetterGrade3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbLetterGrade2
+        '
+        Me.tbLetterGrade2.AccessibleName = "tbLetterGrade2"
+        Me.tbLetterGrade2.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade2.Location = New System.Drawing.Point(219, 57)
+        Me.tbLetterGrade2.Name = "tbLetterGrade2"
+        Me.tbLetterGrade2.ReadOnly = True
+        Me.tbLetterGrade2.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade2.TabIndex = 0
+        Me.tbLetterGrade2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbLetterGrade1
+        '
+        Me.tbLetterGrade1.AccessibleName = "tbLetterGrade1"
+        Me.tbLetterGrade1.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbLetterGrade1.Location = New System.Drawing.Point(219, 12)
+        Me.tbLetterGrade1.Name = "tbLetterGrade1"
+        Me.tbLetterGrade1.ReadOnly = True
+        Me.tbLetterGrade1.Size = New System.Drawing.Size(100, 22)
+        Me.tbLetterGrade1.TabIndex = 0
+        Me.tbLetterGrade1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbMark6
+        '
+        Me.tbMark6.AccessibleName = "tbMark6"
+        Me.tbMark6.Location = New System.Drawing.Point(98, 218)
+        Me.tbMark6.Name = "tbMark6"
+        Me.tbMark6.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark6.TabIndex = 12
+        Me.tbMark6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbMark5
+        '
+        Me.tbMark5.AccessibleName = "tbMark5"
+        Me.tbMark5.Location = New System.Drawing.Point(98, 175)
+        Me.tbMark5.Name = "tbMark5"
+        Me.tbMark5.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark5.TabIndex = 10
+        Me.tbMark5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbMark4
+        '
+        Me.tbMark4.AccessibleName = "tbMark4"
+        Me.tbMark4.Location = New System.Drawing.Point(98, 134)
+        Me.tbMark4.Name = "tbMark4"
+        Me.tbMark4.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark4.TabIndex = 8
+        Me.tbMark4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbMark3
+        '
+        Me.tbMark3.AccessibleName = "tbMark3"
+        Me.tbMark3.Location = New System.Drawing.Point(98, 96)
+        Me.tbMark3.Name = "tbMark3"
+        Me.tbMark3.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark3.TabIndex = 6
+        Me.tbMark3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbMark2
+        '
+        Me.tbMark2.AccessibleName = "tbMark2"
+        Me.tbMark2.Location = New System.Drawing.Point(98, 58)
+        Me.tbMark2.Name = "tbMark2"
+        Me.tbMark2.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark2.TabIndex = 4
+        Me.tbMark2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'tbMark1
+        '
+        Me.tbMark1.AccessibleName = "tbMark1"
+        Me.tbMark1.Location = New System.Drawing.Point(98, 12)
+        Me.tbMark1.Name = "tbMark1"
+        Me.tbMark1.Size = New System.Drawing.Size(100, 22)
+        Me.tbMark1.TabIndex = 2
+        Me.tbMark1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.Course6MarkToolTip.SetToolTip(Me.tbMark1, "Mark for course 6")
+        Me.CourseMark1ToolTip.SetToolTip(Me.tbMark1, "Mark for course 1")
+        Me.CalculateButtonToolTip.SetToolTip(Me.tbMark1, "Calculate semester average")
+        Me.CourseMark3ToolTip.SetToolTip(Me.tbMark1, "Mark for course 3")
+        Me.CourseMark2ToolTip.SetToolTip(Me.tbMark1, "Mark for course 2")
+        Me.Course5MarkToolTip.SetToolTip(Me.tbMark1, "Mark for course 5")
+        Me.Course4MarkToolTip.SetToolTip(Me.tbMark1, "Mark for course 4")
+        Me.SemesterGradeToolTip.SetToolTip(Me.tbMark1, "Average semester grade")
+        Me.ResetButtonToolTip.SetToolTip(Me.tbMark1, "Click to reset the form")
+        Me.ExitButtonToolTip.SetToolTip(Me.tbMark1, "Click to exit the form")
+        '
+        'lbCourse6
+        '
+        Me.lbCourse6.AccessibleName = "lbCourse6"
+        Me.lbCourse6.Location = New System.Drawing.Point(11, 218)
+        Me.lbCourse6.Name = "lbCourse6"
+        Me.lbCourse6.Size = New System.Drawing.Size(69, 17)
+        Me.lbCourse6.TabIndex = 11
+        Me.lbCourse6.Text = "Course 6:"
+        Me.lbCourse6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbCourse5
+        '
+        Me.lbCourse5.AccessibleName = "lbCourse5"
+        Me.lbCourse5.Location = New System.Drawing.Point(11, 175)
+        Me.lbCourse5.Name = "lbCourse5"
+        Me.lbCourse5.Size = New System.Drawing.Size(69, 17)
+        Me.lbCourse5.TabIndex = 9
+        Me.lbCourse5.Text = "Course 5:"
+        Me.lbCourse5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbCourse4
+        '
+        Me.lbCourse4.AccessibleName = "lbCourse4"
+        Me.lbCourse4.Location = New System.Drawing.Point(11, 134)
+        Me.lbCourse4.Name = "lbCourse4"
+        Me.lbCourse4.Size = New System.Drawing.Size(69, 17)
+        Me.lbCourse4.TabIndex = 7
+        Me.lbCourse4.Text = "Course 4:"
+        Me.lbCourse4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbCourse3
+        '
+        Me.lbCourse3.AccessibleName = "lbCourse3"
+        Me.lbCourse3.Location = New System.Drawing.Point(11, 96)
+        Me.lbCourse3.Name = "lbCourse3"
+        Me.lbCourse3.Size = New System.Drawing.Size(69, 17)
+        Me.lbCourse3.TabIndex = 5
+        Me.lbCourse3.Text = "Course 3:"
+        Me.lbCourse3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbCourse2
+        '
+        Me.lbCourse2.AccessibleName = "lbCourse2"
+        Me.lbCourse2.Location = New System.Drawing.Point(11, 58)
+        Me.lbCourse2.Name = "lbCourse2"
+        Me.lbCourse2.Size = New System.Drawing.Size(69, 17)
+        Me.lbCourse2.TabIndex = 3
+        Me.lbCourse2.Text = "Course 2:"
+        Me.lbCourse2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbCouse1
+        '
+        Me.lbCouse1.AccessibleName = "lbCourse1"
+        Me.lbCouse1.Location = New System.Drawing.Point(11, 18)
+        Me.lbCouse1.Name = "lbCouse1"
+        Me.lbCouse1.Size = New System.Drawing.Size(69, 17)
+        Me.lbCouse1.TabIndex = 1
+        Me.lbCouse1.Text = "Course 1:"
+        Me.lbCouse1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lbSemester
+        '
+        Me.lbSemester.AccessibleName = "lbSemester"
+        Me.lbSemester.Location = New System.Drawing.Point(8, 257)
+        Me.lbSemester.Name = "lbSemester"
+        Me.lbSemester.Size = New System.Drawing.Size(72, 17)
+        Me.lbSemester.TabIndex = 13
+        Me.lbSemester.Text = "Semester:"
+        Me.lbSemester.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'tbSemester
+        '
+        Me.tbSemester.AccessibleName = "tbSemester"
+        Me.tbSemester.Location = New System.Drawing.Point(98, 257)
+        Me.tbSemester.Name = "tbSemester"
+        Me.tbSemester.ReadOnly = True
+        Me.tbSemester.Size = New System.Drawing.Size(100, 22)
+        Me.tbSemester.TabIndex = 14
+        Me.tbSemester.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Course6MarkToolTip.SetToolTip(Me.tbSemester, "Mark for course 6")
+        Me.CourseMark1ToolTip.SetToolTip(Me.tbSemester, "Mark for course 1")
+        Me.CalculateButtonToolTip.SetToolTip(Me.tbSemester, "Calculate average grade")
+        Me.CourseMark3ToolTip.SetToolTip(Me.tbSemester, "Mark for course 3")
+        Me.CourseMark2ToolTip.SetToolTip(Me.tbSemester, "Mark for course 2")
+        Me.Course5MarkToolTip.SetToolTip(Me.tbSemester, "Mark for course 5")
+        Me.Course4MarkToolTip.SetToolTip(Me.tbSemester, "Mark for course 4")
+        Me.SemesterGradeToolTip.SetToolTip(Me.tbSemester, "Semester average grade")
+        Me.ResetButtonToolTip.SetToolTip(Me.tbSemester, "Resets the form")
+        Me.ExitButtonToolTip.SetToolTip(Me.tbSemester, "Exit the form")
+        '
+        'tbSemesterGrade
+        '
+        Me.tbSemesterGrade.AccessibleName = "tbSemesterGrade"
+        Me.tbSemesterGrade.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbSemesterGrade.Location = New System.Drawing.Point(219, 256)
+        Me.tbSemesterGrade.Name = "tbSemesterGrade"
+        Me.tbSemesterGrade.ReadOnly = True
+        Me.tbSemesterGrade.Size = New System.Drawing.Size(100, 22)
+        Me.tbSemesterGrade.TabIndex = 0
+        Me.tbSemesterGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbMessages
+        '
+        Me.tbMessages.AccessibleName = "tbMessages"
+        Me.tbMessages.BackColor = System.Drawing.SystemColors.Menu
+        Me.tbMessages.Location = New System.Drawing.Point(11, 312)
+        Me.tbMessages.Multiline = True
+        Me.tbMessages.Name = "tbMessages"
+        Me.tbMessages.ReadOnly = True
+        Me.tbMessages.Size = New System.Drawing.Size(308, 295)
+        Me.tbMessages.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.AccessibleName = "btnCalculate"
+        Me.btnCalculate.Location = New System.Drawing.Point(61, 613)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(85, 23)
+        Me.btnCalculate.TabIndex = 15
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.AccessibleName = "btnReset"
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(153, 613)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 16
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.AccessibleName = "btnExit"
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(243, 613)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 17
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmSemesterAverage
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AccessibleName = "frmSemesterAverage"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(338, 652)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.tbMessages)
+        Me.Controls.Add(Me.tbSemesterGrade)
+        Me.Controls.Add(Me.tbSemester)
+        Me.Controls.Add(Me.lbSemester)
+        Me.Controls.Add(Me.tbLetterGrade6)
+        Me.Controls.Add(Me.tbLetterGrade5)
+        Me.Controls.Add(Me.tbLetterGrade4)
+        Me.Controls.Add(Me.tbLetterGrade3)
+        Me.Controls.Add(Me.tbLetterGrade2)
+        Me.Controls.Add(Me.tbLetterGrade1)
+        Me.Controls.Add(Me.tbMark6)
+        Me.Controls.Add(Me.tbMark5)
+        Me.Controls.Add(Me.tbMark4)
+        Me.Controls.Add(Me.tbMark3)
+        Me.Controls.Add(Me.tbMark2)
+        Me.Controls.Add(Me.tbMark1)
+        Me.Controls.Add(Me.lbCourse6)
+        Me.Controls.Add(Me.lbCourse5)
+        Me.Controls.Add(Me.lbCourse4)
+        Me.Controls.Add(Me.lbCourse3)
+        Me.Controls.Add(Me.lbCourse2)
+        Me.Controls.Add(Me.lbCouse1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmSemesterAverage"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Semester Grades"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbLetterGrade6 As TextBox
+    Friend WithEvents tbLetterGrade5 As TextBox
+    Friend WithEvents tbLetterGrade4 As TextBox
+    Friend WithEvents tbLetterGrade3 As TextBox
+    Friend WithEvents tbLetterGrade2 As TextBox
+    Friend WithEvents tbLetterGrade1 As TextBox
+    Friend WithEvents tbMark6 As TextBox
+    Friend WithEvents tbMark5 As TextBox
+    Friend WithEvents tbMark4 As TextBox
+    Friend WithEvents tbMark3 As TextBox
+    Friend WithEvents tbMark2 As TextBox
+    Friend WithEvents tbMark1 As TextBox
+    Friend WithEvents lbCourse6 As Label
+    Friend WithEvents lbCourse5 As Label
+    Friend WithEvents lbCourse4 As Label
+    Friend WithEvents lbCourse3 As Label
+    Friend WithEvents lbCourse2 As Label
+    Friend WithEvents lbCouse1 As Label
+    Friend WithEvents lbSemester As Label
+    Friend WithEvents tbSemester As TextBox
+    Friend WithEvents tbSemesterGrade As TextBox
+    Friend WithEvents tbMessages As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents SemesterGradeToolTip As ToolTip
+    Friend WithEvents Course6MarkToolTip As ToolTip
+    Friend WithEvents Course5MarkToolTip As ToolTip
+    Friend WithEvents Course4MarkToolTip As ToolTip
+    Friend WithEvents CourseMark3ToolTip As ToolTip
+    Friend WithEvents CourseMark2ToolTip As ToolTip
+    Friend WithEvents CourseMark1ToolTip As ToolTip
+    Friend WithEvents CalculateButtonToolTip As ToolTip
+    Friend WithEvents ResetButtonToolTip As ToolTip
+    Friend WithEvents ExitButtonToolTip As ToolTip
+End Class
---
+++ SemesterAverage/SemesterAverage.vb
@@ -0,0 +1,203 @@
+﻿''Author: Trevor Andrus
+''Date: 2018-02-17
+''Name: SemesterAverage.vb
+''Purpose: This form allows the user to enter 6 grades and shows them the letter grades for each as well as the semester average
+
+Option Strict On
+Public Class frmSemesterAverage
+
+    'Creates variables
+    Private Const NUM_COURSES As Integer = 6
+    Private inputValidated As Double
+    Private letterGrade As String
+    Dim GradesInput(6) As TextBox
+    Dim Grades(6) As Double
+    Private semesterTotal As Double
+    Private mark1Valid, mark2Valid, mark3Valid, mark4Valid, mark5Valid, mark6Valid As Boolean
+
+
+    ''These 6 Sub's for the 6 input textboxes determine if the input is valid.
+    ''If it is we call the lette grade function and display that as well as add it into our array and set its valid boolean to true
+    ''If it isn't we display and error message and set focus to the text box
+    Private Sub tbMark1_LostFocus(sender As Object, e As EventArgs) Handles tbMark1.Leave
+        If ValidateInput(tbMark1.Text) = True Then
+            tbLetterGrade1.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(0) = tbMark1
+            mark1Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 1 is a number from 0 - 100." + vbNewLine
+            tbMark1.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+
+    Private Sub tbMark2_LostFocus(sender As Object, e As EventArgs) Handles tbMark2.Leave
+        If ValidateInput(tbMark2.Text) = True Then
+            tbLetterGrade2.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(1) = tbMark2
+            mark2Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 2 is a number from 0 - 100." + vbNewLine
+            tbMark2.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+
+    Private Sub tbMark3_LostFocus(sender As Object, e As EventArgs) Handles tbMark3.Leave
+        If ValidateInput(tbMark3.Text) = True Then
+            tbLetterGrade3.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(2) = tbMark3
+            mark3Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 3 is a number from 0 - 100." + vbNewLine
+            tbMark3.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+    Private Sub tbMark4_LostFocus(sender As Object, e As EventArgs) Handles tbMark4.Leave
+        If ValidateInput(tbMark4.Text) = True Then
+            tbLetterGrade4.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(3) = tbMark4
+            mark4Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 4 is a number from 0 - 100." + vbNewLine
+            tbMark4.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+
+    Private Sub tbMark5_LostFocus(sender As Object, e As EventArgs) Handles tbMark5.Leave
+        If ValidateInput(tbMark5.Text) = True Then
+            tbLetterGrade5.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(4) = tbMark5
+            mark5Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 5 is a number from 0 - 100." + vbNewLine
+            tbMark5.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+    Private Sub tbMark6_LostFocus(sender As Object, e As EventArgs) Handles tbMark6.Leave
+        If ValidateInput(tbMark6.Text) = True Then
+            tbLetterGrade6.Text = CalculateLetterGrade(inputValidated)
+            GradesInput(5) = tbMark6
+            mark6Valid = True
+        Else
+            tbMessages.Text = tbMessages.Text + "ERROR: Please make sure what you input in Course 6 is a number from 0 - 100." + vbNewLine
+            tbMark6.Focus()
+        End If
+        inputValidated = Nothing
+    End Sub
+
+
+
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes form
+        Close()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ''Clears textboxes and labels as well as clearing all variables necessary for further operation
+        tbMark1.Clear()
+        tbMark2.Clear()
+        tbMark3.Clear()
+        tbMark4.Clear()
+        tbMark5.Clear()
+        tbMark6.Clear()
+        tbLetterGrade1.Clear()
+        tbLetterGrade2.Clear()
+        tbLetterGrade3.Clear()
+        tbLetterGrade4.Clear()
+        tbLetterGrade5.Clear()
+        tbLetterGrade6.Clear()
+        tbSemester.Clear()
+        tbSemesterGrade.Clear()
+        tbMessages.Clear()
+        inputValidated = Nothing
+        mark1Valid = Nothing
+        mark2Valid = Nothing
+        mark3Valid = Nothing
+        mark4Valid = Nothing
+        mark5Valid = Nothing
+        mark6Valid = Nothing
+        semesterTotal = 0
+        tbMark1.ReadOnly = False
+        tbMark2.ReadOnly = False
+        tbMark3.ReadOnly = False
+        tbMark4.ReadOnly = False
+        tbMark5.ReadOnly = False
+        tbMark6.ReadOnly = False
+        btnCalculate.Enabled = True
+        For i As Integer = 0 To 5 Step 1
+            Grades(i) = Nothing
+            GradesInput(i) = Nothing
+        Next
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If mark1Valid = True And mark2Valid = True And mark3Valid = True And mark4Valid = True And mark5Valid = True And mark6Valid = True Then
+            ''Loops through the array converting to doubles and adding to a total to be used fro average calculation
+            For i As Integer = 0 To 5 Step 1
+                Grades(i) = Convert.ToDouble(GradesInput(i).Text)
+                semesterTotal += Grades(i)
+            Next
+            ''Calculates the average and letter grade and displays them in the appropriate controls
+            semesterTotal = semesterTotal / NUM_COURSES
+            semesterTotal = Math.Round(semesterTotal, 2)
+            tbSemester.Text = semesterTotal.ToString
+            tbSemesterGrade.Text = CalculateLetterGrade(semesterTotal)
+            ''Disables input text boxes and caluclate button until the user presses the reste button
+            tbMark1.ReadOnly = True
+            tbMark2.ReadOnly = True
+            tbMark3.ReadOnly = True
+            tbMark4.ReadOnly = True
+            tbMark5.ReadOnly = True
+            tbMark6.ReadOnly = True
+            btnCalculate.Enabled = False
+        End If
+    End Sub
+
+    Function ValidateInput(input As String) As Boolean
+        ''Determines if input is a double and within the range of 0 - 100 if so returns true otherwise returns false
+        If Double.TryParse(input, inputValidated) And inputValidated >= 0 And inputValidated <= 100 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Function CalculateLetterGrade(grade As Double) As String
+        ''Finds which grade range the given value is and then returns it
+        If grade >= 90 Then
+            letterGrade = "A+"
+        ElseIf grade >= 85 Then
+            letterGrade = "A"
+        ElseIf grade >= 80 Then
+            letterGrade = "A-"
+        ElseIf grade >= 77 Then
+            letterGrade = "B+"
+        ElseIf grade >= 73 Then
+            letterGrade = "B"
+        ElseIf grade >= 70 Then
+            letterGrade = "B-"
+        ElseIf grade >= 67 Then
+            letterGrade = "C+"
+        ElseIf grade >= 63 Then
+            letterGrade = "C"
+        ElseIf grade >= 60 Then
+            letterGrade = "C-"
+        ElseIf grade >= 57 Then
+            letterGrade = "D+"
+        ElseIf grade >= 53 Then
+            letterGrade = "D"
+        ElseIf grade >= 50 Then
+            letterGrade = "D-"
+        Else
+            letterGrade = "F"
+        End If
+        Return letterGrade
+    End Function
+End Class
